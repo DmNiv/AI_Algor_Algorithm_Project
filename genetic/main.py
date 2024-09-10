ed55dae9a5ba977797344d0ecbad742dd6c3c642
Pedro Castro <pedro7mirand@gmail.com>
2024-09-09 23:30:09 -0300

    deletando o algoritmo genético

--- genetic/main.py
+++
@@ -1,6 +0,0 @@
-from genetic_algorithm import *
-
-
-
-melhor_solucao, geracoes, colisao = algoritmo_genetico()
-print(f"Melhor solução: {melhor_solucao} em {geracoes} gerações com {colisao} colisões")
